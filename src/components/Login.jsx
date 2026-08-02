6237405a57125dee5c0f6f766156399dd5117cf0
Rohit Sah <rohitsahps309@gmail.com>
2026-08-02 18:24:10 +0530

    update Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react'
+import {useState, useRef} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import authService from '../appwrite/auth'
 import { login as authLogin } from '../store/authSlice'
